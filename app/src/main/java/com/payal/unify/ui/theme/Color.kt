fc8f0f55e0f006af17a67afdebb35b460f8e1f9e
payalsonipali <payalsonipali@gmail.com>
2023-09-12 10:47:10 +0530

    minor changes

--- app/src/main/java/com/payal/unify/ui/theme/Color.kt
+++ app/src/main/java/com/payal/unify/ui/theme/Color.kt
@@ -4,6 +4,5 @@ import androidx.compose.ui.graphics.Color
 
 val light_orange = Color(0xFFFFF2E4)
 val orange = Color(0XFFFF9C2B)
-val off_white = Color(0xFFF0F0F0)
 val light_gray = Color(0xFFE6E5E5)
 val gray = Color(0xFF6D6A6A)
