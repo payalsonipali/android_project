c0aad302cc6270c1e732d7f6c3c2d38ea10b6fd8
payalsonipali <payalsonipali@gmail.com>
2023-09-12 11:02:56 +0530

    added json data

--- app/src/main/java/com/payal/unify/repository/MockRepository.kt
+++ app/src/main/java/com/payal/unify/repository/MockRepository.kt
@@ -1,33 +1,38 @@
 package com.payal.unify.repository
 
-import com.payal.unify.R
+import com.google.gson.Gson
 import com.payal.unify.model.ApiResponse
-import com.payal.unify.model.CardItem
 
 class MockRepository {
+
+    private val json = """
+        {
+            "expiry": "29-May-2024",
+            "amountDescription": "UGX 10,000",
+            "frequency": "As Presented",
+            "cardItems": [
+                {
+                    "imageUrl": "https://apptestsoko.s3.ap-south-1.amazonaws.com/assets/a.png",
+                    "isSelected": true,
+                    "title": "Airtel Money - XXX897"
+                },
+                {
+                    "imageUrl": "https://apptestsoko.s3.ap-south-1.amazonaws.com/assets/fp.png",
+                    "isSelected": false,
+                    "title": "Flexi Pay - XXX897"
+                },
+                {
+                    "imageUrl": "https://apptestsoko.s3.ap-south-1.amazonaws.com/assets/m.png",
+                    "isSelected": false,
+                    "title": "MTN - XXX897"
+                }
+            ]
+        }
+    """.trimIndent()
+
+    private val gson = Gson()
+
     fun getApiResponse(): ApiResponse {
-        // Simulate fetching data from a network API
-        return ApiResponse(
-            expiry = "29-May-2024",
-            amountDescription = "UGX 10,000",
-            frequency = "As Presented",
-            cardItems = listOf(
-                CardItem(
-                    imageUrl = "https://apptestsoko.s3.ap-south-1.amazonaws.com/assets/a.png",
-                    isSelected = true,
-                    title = "Airtel Money - XXX897"
-                ),
-                CardItem(
-                    imageUrl = "https://apptestsoko.s3.ap-south-1.amazonaws.com/assets/fp.png",
-                    isSelected = false,
-                    title = "Flexi Pay - XXX897"
-                ),
-                CardItem(
-                    imageUrl = "https://apptestsoko.s3.ap-south-1.amazonaws.com/assets/m.png",
-                    isSelected = false,
-                    title = "MTN - XXX897"
-                )
-            )
-        )
+        return gson.fromJson(json, ApiResponse::class.java)
     }
 }
